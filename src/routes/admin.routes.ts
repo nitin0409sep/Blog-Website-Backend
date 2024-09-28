d3e9dca31cf1eaee297a8c3f12b44d5e69baf2f7
nitin0409verma <nitin.verma@remotestate.com>
2024-09-29 01:16:06 +0530

    created api for user profile

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { getUser } from "../controllers/admin/admin.controller";
 import { roleBasedAuthMiddleware } from "../middlewares/auth.middleware";
+import { getUsersController } from "../controllers/user/users.controller";
 
 const route = Router();
+
+// Get ALl Users
+route.get('/getUsers', roleBasedAuthMiddleware('Admin'), getUsersController);
 
 route.get('/userList', roleBasedAuthMiddleware("Admin"), getUser);
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getUsersController } from "../controllers/user/users.controller";
+import { getUserProfile, getUsersController } from "../controllers/user/users.controller";
 import { roleBasedAuthMiddleware } from "../middlewares/auth.middleware";
 import { PostRoute } from "./posts.route";
 
 // Router
 const route = Router();
 
-// Get ALl Users
-route.get('/getUsers', roleBasedAuthMiddleware('Admin'), getUsersController);
+// Get User Profile
+route.get('/userProfile', roleBasedAuthMiddleware('User'), getUserProfile);
 
 // User Posts Routes
 route.use('/post', roleBasedAuthMiddleware('User'), PostRoute);
